0c72d0803559104a682cb2b372562d027bff41af
Mosiah Andrade <mosiahassuncao@GMAIL.COM>
2025-03-31 20:03:05 -0300

    aula 31/03

---
+++ 31-03.sql
@@ -0,0 +1,16 @@
+-- SQL: DQL
+SELECT * FROM FUNCIONARIO;	
+select nome as "Funcionário", cpf, dataNasc, genero, email, salario from funcionario;
+
+select nome as "Funcionário", cpf "CPF", 
+	dataNasc "Data de Nascimento", genero "Gênero", 
+    email "E-mail", salario "Salário" 
+		from funcionario;
+        
+select upper( nome ) as "Funcionário", cpf "CPF", 
+	date_format( dataNasc, '%d / %m / %Y' ) "Data de Nascimento", replace(replace(genero, 'F', "Feminino"), 'M', 'Masculino' ) "Gênero", 
+    email "E-mail", format(salario,2 , "de_DE") "Salário" 
+		from funcionario 
+			order by nome;
+        
+        
